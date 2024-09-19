b57401c2b6e40e881a547dc67e3e0d45fc07aae1
Sean McClure <sean.mcclure17@gmail.com>
2024-09-19 16:34:53 -0400

    streamline HeroText for SEO

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -6,14 +6,12 @@ const gradientText = `bg-gradient-to-r from-[#005430] to-[#8C8A8A] bg-clip-text 
 export const HeroText = () => {
   return (
     <>
-      <div className="flex flex-col sm:mt-24 mt-16">
+      <div className="flex flex-col sm:mt-24 mt-16 md:w-[550px]">
         <div className="flex flex-row">
           <h1
             className={`font-bold pb-3 ${inter.className} ${gradientText} text-5xl sm:text-7xl`}
           >
-            <p>Power your</p>
-            <p>website with</p>
-            <p>Google Sheets</p>
+            Power your website with Google Sheets
           </h1>
         </div>
         <h2 className="text-gray-600 sm:max-w-lg font-light text-md sm:text-2xl">

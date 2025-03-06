5fea4d1cfd6952f6e6cfe9d3c5f0db7ea1555f4d
Sean McClure <sean.mcclure17@gmail.com>
2025-03-06 16:01:00 -0500

    Remove testing

--- src/components/GetPremiumLink.tsx
+++ src/components/GetPremiumLink.tsx
@@ -11,7 +11,7 @@ export const GetPremiumLink = ({ email }: { email: string }) => {
         type="button"
         className="text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-md text-sm px-3 py-1.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 transition ease-in-out duration-300"
       >
-        Get Premium {config.environment}
+        Get Premium
       </button>
     </Link>
   );

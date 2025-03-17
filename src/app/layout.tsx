05eaa39c26d5de9b9488e80c142ae4a8b0124887
Sean McClure <sean.mcclure17@gmail.com>
2025-03-17 02:41:06 -0400

    update icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,41 @@ export const metadata: Metadata = {
         url: "/favicon.ico",
         type: "image/x-icon",
       },
-      // add /favicon-16x16.png favicon-32x32.png, favicon-96x96.png, android-chrome-192x192.png with type: image/png
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-128x128.png",
+        sizes: "128x128",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-196x196.png",
+        sizes: "196x196",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
     shortcut: [
       {
@@ -63,19 +97,58 @@ export const metadata: Metadata = {
         type: "image/x-icon",
       },
     ],
-    // apple: [
-    //   {
-    //     url: "/apple-icon-57x57.png",
-    //     sizes: "57x57",
-    //     type: "image/png",
-    //   },
-    //   {
-    //     url: "/apple-icon-60x60.png",
-    //     sizes: "60x60",
-    //     type: "image/png",
-    //   },
-    // add apple-icon-72x72.png, apple-icon-76x76.png, apple-icon-114x114.png, apple-icon-120x120.png, apple-icon-144x144.png, apple-icon-152x152.png, apple-icon-180x180.png
-    // ],
+    apple: [
+      {
+        url: "/apple-touch-icon-57x57.png",
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-60x60.png",
+        sizes: "60x60",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-76x76.png",
+        sizes: "76x76",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-114x114.png",
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-120x120.png",
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-167x167.png",
+        sizes: "167x167",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
   },
 };
 

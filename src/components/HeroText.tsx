ced983446caf3d5bdca9d049c8a3a04169c87c74
sean mcclure <sean.mcclure17@gmail.com>
2025-07-26 13:37:27 -0400

    update hero

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -13,7 +13,8 @@ export const HeroText = () => {
             className={`font-bold pb-3 ${inter.className} ${gradientText} text-5xl sm:text-7xl`}
           >
             {/* Power your website with Google Sheets */}
-            The CMS your team already knows
+            {/* The CMS your team already knows */}
+            Google Drive as your CMS 
           </h1>
         </div>
         <h2 className={`${interLight.className} text-gray-600 font-ultralight text-sm sm:text-xl w-3/4 mx-auto`}>

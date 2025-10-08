dd80191e53e361333beb56bd5c464d64e6185e22
Sean McClure <sean.mcclure17@gmail.com>
2025-10-08 18:31:51 -0400

    remove re-routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,6 @@
 // middleware.ts
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_FILE = /\.(.*)$/; // skip _next/static, favicon.ico, images, etc.
-
 export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-  const host = req.headers.get("host") || "";
-
-  // Skip static/public assets
-  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes("_next")) {
-    return;
-  }
-
-  // Extract subdomain
-  const [subdomain] = host.split(".");
-  if (subdomain === "app") {
-    url.pathname = `/app${url.pathname}`;
-    return NextResponse.rewrite(url);
-  }
-
-  // Otherwise just continue without rewrite
   return;
 }

68072628ccd3407baf730886654c41bb32fa0ca2
Sean McClure <sean.mcclure17@gmail.com>
2025-10-09 08:58:35 -0400

    remove footer dynamic data

--- src/components/LiteFooter.tsx
+++ src/components/LiteFooter.tsx
@@ -1,15 +1,10 @@
-import config from "@/config";
-import { getUserData } from "@/data/fetching";
 import Link from "next/link";
 
 export const LiteFooter = async ({ mt = 20 }: { mt?: number }) => {
-  const userResponse = await getUserData();
-  const userData =
-    userResponse.status === "logged_in" ? userResponse.data : null;
-  const isPremium = userData?.premium;
-
   return (
-    <div className={`flex flex-row mx-auto space-x-6 mt-${mt} text-sm font-light pb-4`}>
+    <div
+      className={`flex flex-row mx-auto space-x-6 mt-${mt} text-sm font-light pb-4`}
+    >
       <Link href="/privacy" className="text-gray-600 hover:text-gray-900">
         Privacy Policy
       </Link>
@@ -22,14 +17,6 @@ export const LiteFooter = async ({ mt = 20 }: { mt?: number }) => {
       >
         Source Code
       </Link>
-      {isPremium && (
-        <Link
-          href={`https://billing.stripe.com/p/login/${config.stripeCustPortalId}`}
-          className="text-gray-600 hover:text-gray-900"
-        >
-          Manage My Subscription
-        </Link>
-      )}
     </div>
   );
 };
